bae203fee089bbc30c664dde64b6310b874df113
rohank07 <rohank_17@hotmail.ca>
2018-10-06 16:20:00 -0500

    Plants,Sunflower,Shooter

---
+++ PlantsVsZombies/src/Model/Shooter.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Shooter extends Plants {
+
+	private final int health = 20;
+	private final int cost  = 5;
+	private int sunPoints;
+	
+	public Shooter(int health , int cost){
+		super(health, cost);
+		
+	}
+
+}
--- PlantsVsZombies/src/Model/Sunflower.java
+++ PlantsVsZombies/src/Model/Sunflower.java
@@ -1,5 +1,23 @@
 package Model;
 
-public class Sunflower {
+public class Sunflower extends Plants {
+
+	
+	
+	private final int health = 10;
+	private final int cost  = 1;
+	private int sunPoints;
+	public Sunflower(int health, int cost){
+		super(health, cost);
+		
+	}
+	
+	private void generateSunPoint(){
+		
+		sunPoints = + 10;
+		
+	}
+
+
 
 }
