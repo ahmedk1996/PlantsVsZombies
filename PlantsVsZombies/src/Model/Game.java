632967eca793cfacfcc34c4e9172ed01af721cac
AlyKhan <AlyKhan@dhcp-84-60.cu-internet.carleton.ca>
2018-10-06 15:11:24 -0400

    created and implemented some methods in game class, implemented gameGrid in layout

--- PlantsVsZombies/src/Model/Game.java
+++ PlantsVsZombies/src/Model/Game.java
@@ -1,5 +1,38 @@
 package Model;
 
 public class Game {
+	Layout layout = new Layout();
+	
+	public Game () {
+		
+	}
+	
+	public void start() {
+		layout.createGrid();
+	}
+	
+	public void start(int rows, int colomns) {
+		layout.createGrid(rows, colomns);
+	}
+	
+	public void redo() {
+		
+	}
+	
+	public void undo() {
+		
+	}
+	
+	public void save() {
+		
+	}
+	
+	public void load() {
+		
+	}
+	
+	public static void main(String args[]) {
 
+	}
+	
 }
--- PlantsVsZombies/src/Model/Layout.java
+++ PlantsVsZombies/src/Model/Layout.java
@@ -1,5 +1,63 @@
 package Model;
 
+import java.util.Arrays;
+
 public class Layout {
 
+	public void createGrid() {
+		int j = 0;
+		Object[][] gameGrid = new Object [5][7];
+		System.out.print(" 0  1  2  3  4  5  6  ");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("  ");
+			for (int k = 0; k<7; k++) {
+				System.out.print(" - ");
+				if(k == 6) {
+					
+					System.out.print(j++);
+				}
+				
+			}
+		}
+	}
+	
+	public void createGrid(int rows, int colomns) {
+		int j = 0;
+		Object[][] gameGrid = new Object [rows][colomns];
+		
+		for (int num = 0; num < colomns; num++) {
+			System.out.print(" " + num + " ");
+		}
+		//System.out.print(" 0  1  2  3  4  5  6  ");
+		for (int i = 0; i < rows; i++) {
+			System.out.println("  ");
+			for (int k = 0; k<colomns; k++) {
+				System.out.print(" - ");
+				if(k == colomns-1) {
+					
+					System.out.print(j++);
+				}
+				
+			}
+		}
+	}
+	
+	
+	public static void main (String args[]) {
+		Layout layout1 = new Layout();
+		layout1.createGrid();
+		
+		System.out.println("  ");
+		System.out.println("  ");
+		
+		Layout layout2 = new Layout();
+		layout2.createGrid(5, 7);
+		
+		System.out.println("  ");
+		System.out.println("  ");
+		
+		Layout layout3 = new Layout();
+		layout3.createGrid(7, 8);
+	}
+	
 }
