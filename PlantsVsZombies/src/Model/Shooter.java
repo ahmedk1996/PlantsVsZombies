0f4f665bb24f6788c3bf88141e19b5d6cd11da66
rohank07 <rohank_17@hotmail.ca>
2018-10-08 13:23:41 -0400

    changed cost of shooter

--- PlantsVsZombies/src/Model/Shooter.java
+++ PlantsVsZombies/src/Model/Shooter.java
@@ -3,7 +3,7 @@ package Model;
 public class Shooter extends Plants {
 
 	private final int health = 20;
-	private final int cost  = 5;
+	private final int cost  = 100;
 	private int sunPoints;
 	
 	public Shooter(int health , int cost){
