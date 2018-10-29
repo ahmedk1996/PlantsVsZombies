76d346dfc51049a09a9992859da1b89cbc903f68
Hunho <hunhoha@cmail.carleton.ca>
2018-10-29 15:32:47 -0400

    Adding Help

--- PlantsVsZombies/src/Model/Game.java
+++ PlantsVsZombies/src/Model/Game.java
@@ -117,7 +117,7 @@ public class Game {
 			System.out.println("1).Easy --- 2).Medium --- 3).Hard --- 9) Help");
 			reader = new Scanner(System.in); 
 			int mode = reader.nextInt();
-			/*if(mode == 9) {
+			if(mode == 9) {
 				try {
 					loadHelptxt();
 				} catch (FileNotFoundException e) {
@@ -125,7 +125,7 @@ public class Game {
 					e.printStackTrace();
 				}
 				return gameDifficulty();
-			}*/
+			}
 			
 			if (mode == 1) {
 				System.out.println("Easy mode has been selected...Good Luck!");
