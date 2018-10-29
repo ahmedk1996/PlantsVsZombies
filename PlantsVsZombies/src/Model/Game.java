626645e4a042e738cdcd39b874918090cb8b20af
Hunho <hunhoha@cmail.carleton.ca>
2018-10-29 15:12:24 -0400

    Adding Help Option In the Game
    Load Help.txt and print the text.

--- PlantsVsZombies/src/Model/Game.java
+++ PlantsVsZombies/src/Model/Game.java
@@ -2,6 +2,8 @@ package Model;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;  
 
 public class Game {
 	private Layout layout;
@@ -15,26 +17,26 @@ public class Game {
 		this.store = new PlantStore();
 		zombieCounter = 0;
 	}
-	
+
 	public void start(int gameMode) {
-	
+
 		boolean gamedone = false;
 		if(!(zombieCounter <=1)) {
-		layout.placeSpawnZombieOnGrid(layout);
+			layout.placeSpawnZombieOnGrid(layout);
 		}
 		layout.print();
 		store.purchaseStartOfGame(layout , true);
-		
+
 		while(gamedone == false) {
 			if(!(zombieCounter <= 1)) {
 				layout.placeSpawnZombieOnGrid(layout);
-				}
+			}
 			zombieCounter--;
 			layout.print();
 			store.purchaseStartOfGame(layout , false);
 			Action action = new Action(store);
 			layout.setGameGrid(action.startAction(layout,gameTurns));
-		
+
 			/*if(action.isGameOver(layout)) {
 				System.out.println("GAME IS OVER!!! LOSER");
 			}*/
@@ -44,7 +46,7 @@ public class Game {
 			layout.print();
 		}
 	}
-	
+
 	public void start(int rows, int colomns) {
 		layout.createGrid(rows, colomns);
 	}
@@ -59,8 +61,8 @@ public class Game {
 				return true;
 			}
 		}
-	
-		
+
+
 		return false;
 	}*/
 	public Boolean gameClear(Layout layout) {
@@ -74,8 +76,8 @@ public class Game {
 		}
 		return flag;
 	}
-	
-	public void promptStart() {
+
+	public void promptStart(){
 		int gameMode = gameDifficulty();
 		reader = new Scanner(System.in);  
 		System.out.println("Below is a 5x7 grid layout:");
@@ -92,69 +94,93 @@ public class Game {
 			System.out.println("Hard mode selected. Zombie types include: ");
 			zombieCounter = 8;
 		}
-		
-		
+
+		if(gameMode == 9) {
+
+
+		}
 		start(gameMode);
 	}
 
+	public void loadHelptxt() throws FileNotFoundException {
+		String toString = "";
+		try{
+			Scanner txtFile	= new Scanner(new FileReader("Help.txt"));
+			 while(txtFile.hasNextLine()) {
+		            toString= toString+ txtFile.nextLine() + "\n";     
+			 }
+			 System.out.println(toString);
+		}catch(FileNotFoundException e){
+			System.out.println("Help File Doesn't Exist");
+		}
+	}
 
 	public int gameDifficulty() {
 		try {
 			System.out.println("There are 3 levels of difficulty");
-			System.out.println("1).Easy --- 2).Medium --- 3).Hard");
+			System.out.println("1).Easy --- 2).Medium --- 3).Hard --- 9) Help");
 			reader = new Scanner(System.in); 
 			int mode = reader.nextInt();
+			if(mode == 9) {
+				try {
+					loadHelptxt();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return gameDifficulty();
+			}
+			
 			if (mode == 1) {
 				System.out.println("Easy mode has been selected...Good Luck!");
 				//set off variable for game difficulty
 				return 1;
-		}
-		else if (mode == 2) {
-			
-			return 2;
-		}
-		
-		else if(mode == 3) {
-			return 3;
-		}
+			}
+			else if (mode == 2) {
 
-		else {
-			System.out.println("Please make a selection to proceed.");
-			gameDifficulty();
-		}
-		
-		
+				return 2;
+			}
+
+			else if(mode == 3) {
+				return 3;
+			}
+			else {
+				System.out.println("Please make a selection to proceed.");
+				gameDifficulty();
+			}
+
+
 		}
 		catch (InputMismatchException e ) {
-			
+
 		}
 		//user input
 		return 0 ;
 	}
 	public void redo() {
-		
+
 	}
-	
+
 	public void undo() {
-		
+
 	}
-	
+
 	public void save() {
-		
+
 	}
-	
+
 	public void load() {
-		
+
 	}
-	
-	
+
+
 	public static void main(String args[]) {
-		
+
 		Layout layout = new Layout();
 		PlantStore store = new PlantStore();
 		// We will test wave by after using beginWave = new wave(1);
 		Game game = new Game(layout, store);
-		
+
 		game.promptStart();
 
 	}
