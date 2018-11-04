d03348431d3fb891d714552aa501fa0b23473dfb
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2018-11-04 16:31:20 -0500

    Some more updates

--- PlantsVsZombies/src/Plant/ShootingPlantTest.java
+++ PlantsVsZombies/src/Plant/ShootingPlantTest.java
@@ -2,15 +2,60 @@ package Plant;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class ShootingPlantTest extends TestCase{
 
+	@Before
+	public void setUp() {
+		//weapon = new Weapon();
+		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testgetName() {
+	
+	}
+
+	@Test
+	public void testsetName() {
+	}
+
+	@Test
+
+	public void testgetWeaponSpeed() {
+	}
+
+	@Test
+	public void testsetWeaponSpeed() {
+	}
+
+	@Test
+	public void testgetWeaponresistant() {
+		
+	}
+	
+	@Test
+	public void testsetWeaponresistant() {
+	}
+
+	@Test
+	public void testgetWeaponhealth() {
+	}
+
+	@Test
+	public void testsetWeaponhealth() {
+	}
+
+	@Test
+	public void testgetWeaponattackDamage() {
+	}
+
+	@Test
+	public void testsetWeaponattackDamage() {
 	}
 
 }
