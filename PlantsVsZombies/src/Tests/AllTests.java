4ccab71d554c9e2ebbdf8b209512d5f3249a9b76
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2018-11-04 16:39:23 -0500

    AllTests

--- PlantsVsZombies/src/Tests/AllTests.java
+++ PlantsVsZombies/src/Tests/AllTests.java
@@ -1,5 +1,25 @@
-package Tests;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-public class AllTests {
+public class AllTests extends TestCase {
 
+ public static void main(String[] args) {
+ junit.textui.TestRunner.run(AllTests.class);
+ }
+ 
+ public static Test suite() {
+ TestSuite suite = new TestSuite("Test for com.saorsa.nowplaying.tests");
+ suite.addTest(new TestSuite(ActionTest.class));
+ suite.addTest(new TestSuite(GameTest.class));
+ suite.addTest(new TestSuite(LayoutTest.class));
+ suite.addTest(new TestSuite(PlantStoreTest.class));
+ suite.addTest(new TestSuite(PrintGridTest.class));
+ suite.addTest(new TestSuite(ShootingPlantTest.class));
+ suite.addTest(new TestSuite(SunflowerTest.class));
+ suite.addTest(new TestSuite(TurnTest.class));
+ suite.addTest(new TestSuite(WalkingZombieTest.class));
+ suite.addTest(new TestSuite(waveTest.class));
+ return suite;
+ }
 }
