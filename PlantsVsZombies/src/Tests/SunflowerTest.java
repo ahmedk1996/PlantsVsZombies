85328e68b65b2ebdf44258b9a75bfa6f15d51fad
Aly Khan <ak.barolia@hotmail.com>
2018-11-07 17:42:50 -0500

    no important changes

--- PlantsVsZombies/src/Tests/SunflowerTest.java
+++ PlantsVsZombies/src/Tests/SunflowerTest.java
@@ -13,7 +13,7 @@ public class SunflowerTest extends TestCase{
 	@Test
 	public void test() { //this test checks the getCost method in Sunflower class
 		Sunflower s1 = new Sunflower();
-		assertEquals("Should pass if the cost of a sunflower plant is 50", 50,s1.getCost());
+		assertEquals("Should pass if the cost of a sunflower plant is 50", 50, s1.getCost());
 	}
 
 	
