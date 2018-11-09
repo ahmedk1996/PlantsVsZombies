0534c86776bc4088f4fbc05560b5798403887401
Hunho <hunhoha@cmail.carleton.ca>
2018-11-08 20:59:25 -0500

    CoolDown Is Working Properly!!
    YESS

--- PlantsVsZombies/src/Model/CoolDownQueue.java
+++ PlantsVsZombies/src/Model/CoolDownQueue.java
@@ -2,18 +2,21 @@
 package Model;
 
 
+import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import Plant.Plants;
+import Plant.ShootingPlant;
+import Plant.Sunflower;
 
 public class CoolDownQueue implements Observer{
 	String name;
-	PriorityQueue<Plants> queue; 
+	Queue<Plants> queue; 
 	
 	public CoolDownQueue(Plants plant) {
-		queue = new PriorityQueue<Plants>();
+		queue = new LinkedList<Plants>();
 		this.name = plant.getName();
 		addCooldown(plant);
 	}
@@ -25,16 +28,30 @@ public class CoolDownQueue implements Observer{
 	 */
 	
 	public void addCooldown(Plants plant) {
-		for(int i=0;i<plant.getCooldown();i++) {
+		
+		for(int i=0; i<plant.getCooldown(); i++) {
 			queue.add(plant);
 		}
+		
+		/**
+		if(plant instanceof ShootingPlant) {
+			for(int i=0; i<plant.getCooldown(); i++) {
+				queue.add(new ShootingPlant());
+			}
+		}else {
+			for(int i=0; i<plant.getCooldown(); i++) {
+				queue.add(new Sunflower());
+			}
+		}**/
+		
+		
 		System.out.println(plant.getName() + "'s Cool Down : " + getRemaining());
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		queue.remove();
+		queue.poll();
 		
 	}
 	public String getName() {
