ec4b297eb28079bac9a108638a53f7a6d949856b
Hunho <hunhoha@cmail.carleton.ca>
2018-11-24 22:47:27 -0500

    TimeLine Test Case

---
+++ src/Tests/TimeLineTest.java
@@ -0,0 +1,70 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import Model.Game;
+import junit.framework.TestCase;
+
+public class TimeLineTest extends TestCase{
+	TimeLine tl ;
+	Game game ;
+	@Before
+	public void setUp() {
+		tl = new TimeLine();
+		game= new Game();
+	}
+
+	@Test
+	//public void addNext(Game game)
+	public void test() {
+		
+		tl.addNext(game);
+		
+		assertTrue("What is stored in game should be game",tl.undo() instanceof Game);
+	}
+	
+	@Test
+	//public boolean isUndoAvailable()
+	public void test1() {
+		tl = new TimeLine();
+		
+		assertFalse("should return false",tl.isUndoAvailable());
+	}
+	
+	@Test
+	//public boolean isRedoAvailable()
+	public void test2() {
+		tl = new TimeLine();
+		
+		assertFalse("should return false",tl.isRedoAvailable());
+	}
+	
+	@Test
+	//public Game undo()
+	public void test3() {
+		tl = new TimeLine();
+		game= new Game();
+		
+		tl.addNext(game);
+		
+		assertTrue("It should return the same game",tl.undo() instanceof Game);
+	}
+	
+	@Test
+	//public Game undo()
+	public void test4() {
+		tl = new TimeLine();
+		game= new Game();
+		
+		tl.addNext(game);
+		tl.addNext(game);
+		tl.addNext(game);
+		
+		Game temp = tl.undo();
+		assertTrue("It should return the game",tl.redo() instanceof Game);
+		
+	}
+	
+}
