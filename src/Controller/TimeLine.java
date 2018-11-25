188e83baa18f45dd8723ca0e8b15eeac4d1f3400
Hunho <hunhoha@cmail.carleton.ca>
2018-11-24 22:27:50 -0500

    Update TimeLine.java

--- src/Controller/TimeLine.java
+++ src/Controller/TimeLine.java
@@ -11,6 +11,10 @@ import Model.Game;
  * The TimeLine class is a class which contains all the actions used to allow the 
  * user to back and forth in a game (undo/redo)
  * 
+ * The reason why we used data structure, ArrayList is because the storage has to be modifiable.
+ * when the user makes new movement after undo then whatever it is already stored before, must be removed and
+ * saves new statement of the game.
+ * 
  * @author Group 1
  * @since November 4,2018
  * 
@@ -30,9 +34,10 @@ public class TimeLine {
 		currenti = 0;
 	}
 	/*
-	 * need to do "depp copy"
-	 * Taking a game parameter will cause "field-by-field copy,field-for-field copy, or field copy".
-	 * therefore, need to save it independently using serialization there were other ways but I used serialization for next milestone.
+	 * stores the game status in to arraylist.
+	 * @param Game
+	 * @return void - new board with forward state
+	 *
 	 */
 	public void addNext(Game game) {
 
@@ -51,18 +56,16 @@ public class TimeLine {
 		}
 	}
 	/*
-	public void addNext(cabsule c) {
-
-		//Controller(Game game, View view,Layout layout,PlantStore ps, Action action, Turn turn, CoolDown c,int stag)
-
-		if(timeStore.size() > currenti) {
-			timeStore.set(currenti, c );
-			currenti ++;
-		}else{ 
-			timeStore.add(c);
-			currenti ++;
-		}
-	}*/
+	 * need to do "depp copy"
+	 * Taking a game parameter will cause "field-by-field copy,field-for-field copy, or field copy".
+	 * therefore, need to save it independently using serialization there were other ways but I used serialization for next milestone.
+	 *
+	 * 	Lets the user go forward if a undo was preivously selected
+	 *	
+	 *	@param Game
+	 * 	@return Game - new board with forward state
+	 */
+	
 	public Game deepCopy(Game orig) {
 		Game obj = null;
 		try {
