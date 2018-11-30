13099f27d997a759f7adffb4ffa4fa52067266d8
Hunho <hunhoha@cmail.carleton.ca>
2018-11-30 12:18:57 -0500

    Update Builder.java

---
+++ src/Controller/Builder.java
@@ -0,0 +1,98 @@
+package Controller;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import Model.Game;
+import java.util.LinkedList;
+public class Builder {
+
+	private Game game;
+	DocumentBuilderFactory dbFactory;
+	org.w3c.dom.Document doc;
+	DocumentBuilder dBuilder;
+	int count = 0;
+	
+	Queue<Object> waveList;
+	List<Object> wave;
+	int time;
+	public Builder() throws SAXException, IOException, ParserConfigurationException {
+
+		// readFile();
+		// game = new Game();
+		waveList = new LinkedList<Object>();
+	}
+	public boolean isEmpty() {
+		if(waveList.isEmpty()) {
+			return true;
+		}else {
+			return false;
+		}
+		
+		/*if(zombieType == null) {
+			return false;
+		}else return true;*/
+	}
+	public int getTime() {
+		return time;
+	}
+	public List<Object> getWave(){
+		List<Object> poll = (List<Object>) waveList.poll();
+		return poll;
+	}
+	
+	public void readFile() throws SAXException, IOException, ParserConfigurationException {
+
+		File xmlFile = new File("src/custom.xml");
+		dbFactory = DocumentBuilderFactory.newInstance();
+		dBuilder = dbFactory.newDocumentBuilder();
+		doc = dBuilder.parse(xmlFile);
+		doc.getDocumentElement().normalize();
+		
+		NodeList nList = doc.getElementsByTagName("wave");
+		
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			 
+			this.time = Integer.parseInt(((Element)nList.item(0)).getElementsByTagName("startTime").item(0).getTextContent());
+			System.out.println(time);
+			NodeList zombie = ((Element) nList.item(temp)).getElementsByTagName("zombie");
+			
+			for (int i = 0; i < zombie.getLength(); i++) {
+				Node current = zombie.item(i);
+				if (current.getNodeType() == Node.ELEMENT_NODE) {
+			         String zomType = ((Element) current).getElementsByTagName("type").item(0).getTextContent();
+			         wave.add(zomType);
+			         //zombieType.add(zomType);
+			         System.out.println(zomType);
+			         String zombCount = ((Element) current).getElementsByTagName("count").item(0).getTextContent();
+			         int z = Integer.parseInt(zombCount);
+			         wave.add(z);
+			         //String count = zombCount;
+			         
+			         //zombieCount.add(count);
+			         System.out.println(zombCount);
+				}
+			}
+			waveList.add(wave);
+		}
+
+	}
+	
+
+
+}
